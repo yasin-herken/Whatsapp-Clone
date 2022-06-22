7dca962ccab4d4cf124d1289211d1f438d5c1465
yasin-herken <yasin.herken123@gmail.com>
2022-06-22 09:42:05 +0300

    Initial Components Added

---
+++ whatsapp-mern/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Sidebar from './Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+      {/* Chat component*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAdrRMNEhw9dMpPSM3RjQb8UZr3nABgvD0",
+  authDomain: "whatsapp-mern-907fe.firebaseapp.com",
+  projectId: "whatsapp-mern-907fe",
+  storageBucket: "whatsapp-mern-907fe.appspot.com",
+  messagingSenderId: "561325386352",
+  appId: "1:561325386352:web:5b6e0d6e026d4ff4c762b5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
